b0492fbbc82b7e7d2a88462d55e5a7c873ab5d4d
gtunon <gtunon@naevatec.com>
2017-02-09 04:33:27 -0800

    first Commit

---
+++ src/test/java/org/kurento/turorial/helloworld/test/FooTest.java
@@ -0,0 +1,14 @@
+package org.kurento.turorial.helloworld.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FooTest {
+
+	
+	@Test
+	public void testOK() {
+		org.junit.Assert.assertTrue(true);
+	}
+}
